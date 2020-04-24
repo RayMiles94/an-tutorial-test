543cad86f926340fe44f87b3d965904f7b735106
raouf-slnee <raouf.jabri@slnee.com>
2020-04-24 15:56:24 +0100

    13. Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -17,35 +17,40 @@ export interface AuthresponseData {
 })
 export class AuthService {
 
-    constructor(private http:HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
         return this.http.post<AuthresponseData>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDmS1QIGQIVo2TkwZTyO9FWQuSbLmGbbDw'
-        , {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }).pipe(catchError(
-            error => {
-                let errorMessage = 'An unkown error random!';
-                if (!error.error || !error.error.error ) {
-                   return throwError(errorMessage);
-                }
-                switch (error.error.error.message) {
-                    case 'EMAIL_EXISTS': errorMessage = 'This EMAIL already exist'
-                }
-                return throwError(errorMessage);
-            }
-        ));
+            , {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
     }
 
     login(login: string, password: string) {
         return this.http.post<AuthresponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key= AIzaSyDmS1QIGQIVo2TkwZTyO9FWQuSbLmGbbDw',
-        {
-            email: login,
-            password: password,
-            returnSecureToken: true
-        });
+            {
+                email: login,
+                password: password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
+    }
+
+
+    private handleError(errorRES: HttpErrorResponse) {
+        let errorMessage = 'An unkown error random!';
+        if (!errorRES.error || !errorRES.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRES.error.error.message) {
+            case 'EMAIL_EXISTS': errorMessage = 'This EMAIL already exist'; break;
+            case 'EMAIL_NOT_FOUND': errorMessage = 'This is EMAIL is not found in database'; break;
+            case 'INVALID_PASSWORD': errorMessage = 'This is invalid password'; break;
+            case 'USER_DISABLED': errorMessage = 'This user is disabled in database'; break;
+
+        }
+        return throwError(errorMessage);
     }
 }
